090b1d5ab0289a3aeaacf4972aadda1a9af77a23
cwelfley01 <cwelfley01@gmail.com>
2022-07-14 22:01:14 -0400

    url trouble

--- app.py
+++ app.py
@@ -22,18 +22,18 @@ class Books(db.Model):
     author = db.Column(db.String, nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.String, nullable=False)
-    img_url = db.Column(db.String, nullable=True)
+    # img_url = db.Column(db.String, nullable=True)
 
-    def __init__(self, name, price, author, description, img_url):
+    def __init__(self, name, price, author, description):
         self.name = name
         self.price = price
         self.author = author
         self.description = description
-        self.img_url = img_url
+        # self.img_url = img_url
 
 class BookSchema(ma.Schema):
     class Meta:
-        fields = ("id", "name", "price", "author", "description", "img_url")
+        fields = ("id", "name", "price", "author", "description")
 
 book_schema = BookSchema()
 books_schema = BookSchema(many=True)
@@ -44,9 +44,9 @@ def add_book():
     author = request.json.get("author")
     description = request.json.get("description")
     price = request.json.get("price")
-    img_url = request.json.get("img_url")
+    # img_url = request.json.get("img_url")
 
-    record = Books(name, price, author, description, img_url)
+    record = Books(name, price, author, description)
     
     db.session.add(record)
     db.session.commit()
@@ -71,13 +71,13 @@ def book_id(id):
         author = request.json['author']
         price = request.json['price']
         description = request.json['description']
-        img_url = request.json['img_url']
+        # img_url = request.json['img_url']
 
         book.name = name
         book.author = author
         book.price = price
         book.description = description
-        book.img_url = img_url
+        # book.img_url = img_url
 
         db.session.commit()
         return book_schema.jsonify(book)
